c44608b735105a9dd972f9bbc947912926946e3c
shkurmanbek <ckurmanbek@gmail.com>
2019-11-01 01:27:08 +0600

    Add authorize And register not completely

--- project_dl/loginsys/urls.py
+++ project_dl/loginsys/urls.py
@@ -5,5 +5,5 @@ import loginsys.views
 urlpatterns = [
     path('login/', loginsys.views.login),
     path('logout/', loginsys.views.logout),
-
+    path('signup/', loginsys.views.signup),
 ]
--- project_dl/loginsys/views.py
+++ project_dl/loginsys/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect, render_to_response
-from django.template.context_processors import csrf
-from requests import auth
 
+from django.template.context_processors import csrf
+from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
 
 def login(request):
     args = {}
     args.update(csrf(request))
     if request.POST:
-        username = request.POST.get('username', '') # Directly from POST request
-                                                    # without using form as not as in comment form
+        username = request.POST.get('username',
+                                    '')  # Directly from POST request                                       # without using form as not as in comment form
         password = request.POST.get('password', '')
         user = auth.authenticate(username=username, password=password)
         if user is not None:
@@ -20,6 +21,24 @@ def login(request):
     else:
         return render_to_response('login.html', args)
 
+
 def logout(request):
     auth.logout(request)
     return redirect('/')
+
+def signup(request):
+    args = {}
+    args.update(csrf(request))
+    args['forms'] = UserCreationForm
+    if request.POST:
+        newuser_form = UserCreationForm(request.POST)
+        if newuser_form.is_valid():
+            newuser_form.save()
+            newuser = auth.authenticate(username=newuser_form.cleaned_data['username'],
+                                        password=newuser_form.cleaned_data['password2'])
+            auth.login(request, newuser)
+            return redirect('/')
+        else:
+            args['form'] = newuser_form
+    return render_to_response('signup.html', args)
+
